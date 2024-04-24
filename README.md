a2d198b5c201cc53d92ac778dbceae83c9ccc665
enchan <enchant0130@gmail.com>
2024-04-24 19:52:16 +0900

    [Add] initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# miners_tools
+
+## Overview
+
+CutAllとかMineAllとかその辺みたいなのをプラグインだけでやる
+
+## License
+
+This repository is published under [MIT License](LICENSE).
---
+++ src/main/java/me/enchan/miners_tools/MinersTools.java
@@ -0,0 +1,16 @@
+package me.enchan.miners_tools;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public class MinersTools extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("Hello, SpigotMC!");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("See you again, SpigotMC!");
+    }
+}
