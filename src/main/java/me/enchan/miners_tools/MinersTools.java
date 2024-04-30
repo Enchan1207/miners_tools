9c8942c0b7b1604e37fdf69c0c9b688b270cd21d
Enchan <enchant0130@gmail.com>
2024-04-30 19:48:15 +0900

    [Modify #2] register custom event listener

--- src/main/java/me/enchan/miners_tools/MinersTools.java
+++ src/main/java/me/enchan/miners_tools/MinersTools.java
@@ -1,12 +1,20 @@
+//
+// Miners tools メインクラス
+//
 package me.enchan.miners_tools;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-
+/**
+ * Miners tools
+ */
 public class MinersTools extends JavaPlugin {
     @Override
     public void onEnable() {
         getLogger().info("Hello, SpigotMC!");
+
+        // イベントリスナに登録
+        getServer().getPluginManager().registerEvents(new BlockBreakListener(), this);
     }
 
     @Override
